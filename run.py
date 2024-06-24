f5662357e42957cf2356718036004147a59f1b24
Philipp Reuter <reuter.philipp@ymail.com>
2024-06-24 12:52:18 +0200

    Small update, need to update the whole README!

--- run.py
+++ run.py
@@ -1,22 +1,3 @@
 from PyPoRoMOD.account_manager import AccountManager
 
 AccountManager().run()
-
-# import json
-# from pathlib import Path
-# from PyPoRoMOD.enum import speciesEggMoves
-
-
-# # Create a dictionary with the species names and their corresponding move values as strings
-# dict_data = {
-#     species.value: [str(move.value) for move in moves]
-#     for species, moves in speciesEggMoves.items()
-# }
-
-# # Define the directory and file path
-# _DIR = Path(__file__).resolve().parent
-# file_path = _DIR / "speciesEggMoves.json"
-
-# # Write the dictionary to a JSON file
-# with open(file_path, "w") as f:
-#     json.dump(dict_data, f, indent=4)
